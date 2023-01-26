7e7d19f57770589194ddb5e320a8ff6592f5b2c4
yeongcoder <yeongjeseo@yeongjeui-MacBookPro.local>
2023-01-26 14:48:35 +0900

    q04-4 solve

---
+++ ch04/Q04-4/DLinkedList.h
@@ -0,0 +1,38 @@
+#include "Point.h"
+
+#ifndef __D_LINED_LIST_H__
+#define __D_LINED_LIST_H__
+
+#define TRUE    1
+#define FALSE   0
+
+typedef Point* LData;
+
+typedef struct _node
+{
+    LData data;
+    struct _node * next;
+} Node;
+
+typedef struct _linkedList {
+    Node * head;                        // 더미 노드를 가리키는 멤버
+    Node * cur;                         // 참조 및 삭제를 돕는 멤버
+    Node * before;                      // 삭제를 돕는 멤버
+    int numOfData;                      // 저장된 데이터의 수를 기록하기 위한 멤버
+    int (*comp)(LData d1, LData d2);    // 정렬의 기준을 등록하기 위한 멤버
+} LinkedList;
+
+typedef LinkedList List;
+
+void ListInit(List * plist);
+void Linsert(List * plist, LData data);
+
+int LFirst(List * plist, LData * pdata);
+int LNext(List * plist, LData * pdata);
+
+LData LRemove(List * plist);
+int LCount(List * plist);
+
+void SetSortRule(List * plist, int (*comp)(LData d1, LData d2));
+
+#endif
--- ch04/Q04-4/main.c
+++ ch04/Q04-4/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ArrayList.h"
-#include "ArrayList.cpp"
+#include "DLinkedList.h"
+#include "DLinkedList.c"
 #include "Point.h"
-#include "Point.cpp"
+#include "Point.c"
+
+int WhoIsPrecede(LData d1, LData d2){
+    if(d1->xpos < d2->xpos){
+        return 0; // d1이 정렬 순서상 앞선다.
+    } else if(d1->xpos == d2->xpos){
+        if(d1->ypos < d2->ypos){
+            return 0;
+        } else {
+            return 1;
+        }
+    } else {
+        return 1;
+    }
+}
 
 int main(void){
     List list;
@@ -11,6 +25,8 @@ int main(void){
     Point * ppos;
 
     ListInit(&list);
+
+    SetSortRule(&list, WhoIsPrecede);   // 정렬의 기준을 등록한다!
 
     // 4개의 데이터 저장 //////
     ppos = (Point*)malloc(sizeof(Point));
@@ -30,18 +46,6 @@ int main(void){
     LInsert(&list, ppos);
 
     // 저장된 데이터의 출력 //////
-    printf("현재 데이터의 수: %d \n", LCount(&list));
-
-    if(LFirst(&list, &ppos)){
-        ShowPointPos(ppos);
-
-        while(LNext(&list, &ppos)){
-            ShowPointPos(ppos);
-        }
-    }
-    printf("\n");
-
-    // 삭제 후 남은 데이터 전체 출력 //////
     printf("현재 데이터의 수: %d \n", LCount(&list));
 
     if(LFirst(&list, &ppos)){
