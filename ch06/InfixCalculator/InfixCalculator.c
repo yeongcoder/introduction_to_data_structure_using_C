d5a4b3ca31273bd7ae8aabd0078f8b8defd7413e
yeongcoder <yeongjeseo@yeongjeui-MacBookPro.local>
2023-02-02 16:38:04 +0900

    InfixCalculator implement

---
+++ ch06/InfixCalculator/InfixCalculator.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdlib.h>
+#include "InfixToPostfix.h"
+#include "InfixToPostfix.c"
+#include "PostCalculator.h"
+#include "PostCalculator.c"
+
+int EvalInfixExp(char exp[])
+{
+	int len = strlen(exp);
+	int ret;
+	char * expcpy = (char*)malloc(len+1);
+	strcpy(expcpy, exp);
+
+	ConvToRPNExp(expcpy);    
+	ret = EvalRPNExp(expcpy);
+
+	free(expcpy);
+	return ret;
+}
---
+++ ch06/InfixCalculator/PostCalculator.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include <ctype.h>
+#include "ListBaseStack.h"
+#include "ListBaseStack.c"
+
+int EvalRPNExp(char exp[])
+{
+	Stack stack;
+	int expLen = strlen(exp);
+	int i;
+	char tok, op1, op2;
+
+	StackInit(&stack);
+
+	for(i=0; i<expLen; i++)
+	{
+		tok = exp[i];
+
+		if(isdigit(tok))
+		{
+			SPush(&stack, tok - '0');     // ���ڷ� ��ȯ�Ͽ� PUSH!
+		}
+		else
+		{
+			op2 = SPop(&stack);     // ���� ���� ���� �� ��° �ǿ�����!
+			op1 = SPop(&stack);
+
+			switch(tok)
+			{
+			case '+':
+				SPush(&stack, op1+op2);
+				break;
+			case '-':
+				SPush(&stack, op1-op2);
+				break;
+			case '*':
+				SPush(&stack, op1*op2);
+				break;
+			case '/':
+				SPush(&stack, op1/op2);
+				break;
+			}
+		}
+	}
+	return SPop(&stack);
+}
