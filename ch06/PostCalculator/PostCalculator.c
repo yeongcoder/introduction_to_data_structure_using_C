d1ddd8d75cbd26766e2a26a50f35e5570738356a
yeongcoder <yeongjeseo@yeongjeui-MacBookPro.local>
2023-02-02 15:37:16 +0900

    PostCalculator is implemented

---
+++ ch06/PostCalculator/PostCalculator.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <ctype.h>
+#include "../ListBaseStack/ListBaseStack.h"
+#include "../ListBaseStack/ListBaseStack.c"
+
+int EvalRPNExp(char exp[]){
+    Stack stack;
+    int expLen = strlen(exp);
+    int i;
+    char tok, op1, op2;
+
+    StackInit(&stack);
+
+    for(i=0; i<expLen; i++){
+        tok = exp[i];
+
+        if(isdigit(tok)){
+            SPush(&stack, tok - '0');
+        } else {
+            op2 = SPop(&stack);
+            op1 = SPop(&stack);
+
+            switch (tok)
+            {
+            case '+':
+                SPush(&stack, op1+op2);
+                break;
+            case '-':
+                SPush(&stack, op1-op2);
+                break;
+            case '*':
+                SPush(&stack, op1*op2);
+                break;
+            case '/':
+                SPush(&stack, op1/op2);
+                break;
+            }
+        }
+    }
+
+    return SPop(&stack);
+}
