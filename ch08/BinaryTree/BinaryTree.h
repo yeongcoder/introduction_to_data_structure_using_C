ab39b2bc492b36cb597645586a3934707a672008
yeongcoder <yeongjeseo@yeongjeui-MacBookPro.local>
2023-02-07 16:43:54 +0900

    implement BTree

---
+++ ch08/BinaryTree/BinaryTree.h
@@ -0,0 +1,22 @@
+#ifndef __BINARY_TREE_H__
+#define __BINARY_TREE_H__
+
+typedef int BTData;
+
+typedef struct _bTreeNode {
+    BTData data;
+    struct _bTreeNode * left;
+    struct _bTreeNode * right;
+} BTreeNode;
+
+BTreeNode * MakeBTreeNode(void);                // 노드의 생성
+BTData GetData(BTreeNode * bt);                 // 노드에 저장된 데이터를 반환
+void SetData(BTreeNode * bt, BTData data);      // 노드에 데이터를 저장
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt);     // 왼쪽 서브 트리 주소 값 반환
+BTreeNode * GetRightSubTree(BTreeNode * bt);    // 오른쪽 서브 트리 주소 값 반환
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+#endif
