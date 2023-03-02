69ff93bb46aa0403eaba8ccbfbe861e24924c4ed
yeongcoder <yeongjeseo@yeongjeui-MacBookPro.local>
2023-03-02 14:17:38 +0900

    implement chainedTable

---
+++ ch13/ChainedTable/Slot2.h
@@ -0,0 +1,14 @@
+#ifndef __SLOT2_H__
+#define __SLOT2_H__
+
+#include "Person.h"
+
+typedef int Key;        // 주민등록번호
+typedef Person * Value;
+
+typedef struct _slot {
+    Key key;
+    Value val;
+} Slot;
+
+#endif
